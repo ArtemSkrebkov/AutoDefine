e8b34a64ad7de0b1423c7768b4a8019acb665cc7
ArtemSkrebkov <skrebkov.art@gmail.com>
2020-12-10 22:10:54 +0300

    Refactor tests

--- AutoDefineAddon/tests/tests_api.py
+++ AutoDefineAddon/tests/tests_api.py
@@ -9,14 +9,16 @@ sys.path.insert(0, myPath + '/../libs')
 
 import urllib.request
 import settings
+import re
 
 settings.TEST_MODE = True
 settings.DEFINITION_FIELD = 1
 settings.PRONUNCIATION_FIELD = 2
 settings.PHONETIC_TRANSCRIPTION_FIELD = 3
 
-COLLEGIATE_KEY = "47f165c1-346b-410a-b25c-a3611ac762cc"
-SPANISH_KEY = "01c4cc4d-6f84-41e2-9ae0-10cfd5e6277e"
+# put your keys to run tests
+COLLEGIATE_KEY = "YOUR_KEY"
+SPANISH_KEY = "YOUR_KEY"
 
 import cardbuilder
 import autodefine
@@ -26,9 +28,8 @@ def test_empty_entries_if_url_empty():
 
 def test_non_empty_entries_if_url_correct():
     word = "word"
-    KEY = "47f165c1-346b-410a-b25c-a3611ac762cc"
     url = "http://www.dictionaryapi.com/api/v1/references/collegiate/xml/" + \
-                     urllib.parse.quote_plus(word) + "?key=" + KEY
+                     urllib.parse.quote_plus(word) + "?key=" + COLLEGIATE_KEY
     assert cardbuilder.get_entries_from_api("word", url) != []
 
 class MockNote:
@@ -44,25 +45,6 @@ class MockEditor:
     def urlToLink(self, url):
         return url
 
-
-def test_can_serialize_xml_card():
-    settings.MERRIAM_WEBSTER_API_KEY = COLLEGIATE_KEY
-    settings.PREFERRED_DICTIONARY = "COLLEGIATE"
-    word = "test"
-    cardBuilder = cardbuilder.CollegiateCardBuilder(word)
-    cardBuilder.addDefinition()
-    cardBuilder.addPronunciation()
-    cardBuilder.addTranscription()
-    card = cardBuilder.getCard()
-
-    editor = MockEditor()
-    card.serialize(editor)
-
-    assert word == editor.note.fields[0]
-    assert editor.note.fields[settings.DEFINITION_FIELD].find("to put to test or proof") != -1
-    assert editor.note.fields[settings.PRONUNCIATION_FIELD].find("wav") != -1
-    assert editor.note.fields[settings.PHONETIC_TRANSCRIPTION_FIELD].find("test") != -1
-
 def test_can_get_definition():
     settings.MERRIAM_WEBSTER_API_KEY = COLLEGIATE_KEY
     settings.PREFERRED_DICTIONARY = "COLLEGIATE"
@@ -71,57 +53,82 @@ def test_can_get_definition():
     editor.note.fields[0] = word
     autodefine._get_definition(editor, True, True, True)
 
-    print(editor.note.fields)
     assert editor.note.fields[0] == word
     assert editor.note.fields[settings.DEFINITION_FIELD].find("to put or thrust") != -1
     assert editor.note.fields[settings.PRONUNCIATION_FIELD].find("insert") != -1
     assert editor.note.fields[settings.PRONUNCIATION_FIELD].find("wav") != -1
     assert editor.note.fields[settings.PHONETIC_TRANSCRIPTION_FIELD].find("in-ˈsərt") != -1
 
-def test_can_serialize_json_spanish_card():
-    settings.MERRIAM_WEBSTER_API_KEY = SPANISH_KEY
-    settings.PREFERRED_DICTIONARY = "SPANISH"
-    word = "entender"
-    cardBuilder = cardbuilder.SpanishCardBuilder(word)
-    cardBuilder.addDefinition()
-    cardBuilder.addPronunciation()
-    cardBuilder.addTranscription()
-    card = cardBuilder.getCard()
+
+class MockCardBuilder(cardbuilder.CardBuilder):
+    def __init__(self, word):
+        super().__init__(word)
+
+    def addDefinition(self):
+        self._card.fields[settings.DEFINITION_FIELD] = "definition"
+
+    def addTranscription(self):
+        self._card.fields[settings.PHONETIC_TRANSCRIPTION_FIELD] = "transcription"
+
+    def addPronunciation(self):
+        self._card.fields[settings.PRONUNCIATION_FIELD] = "pronunciation"
+
+def test_can_serialize_card():
+    word = "word"
+    card_builder = MockCardBuilder(word)
+    card = card_builder.getCard()
+    card_builder.addDefinition()
+    card_builder.addTranscription()
+    card_builder.addPronunciation()
 
     editor = MockEditor()
     card.serialize(editor)
 
     assert word == editor.note.fields[0]
-    assert editor.note.fields[settings.DEFINITION_FIELD].find("understand") != -1
-    assert editor.note.fields[settings.DEFINITION_FIELD].find("verb") != -1
-    assert editor.note.fields[settings.PRONUNCIATION_FIELD].find("wav") != -1
-    assert editor.note.fields[settings.PRONUNCIATION_FIELD].find("enten") != -1
-    assert editor.note.fields[settings.PHONETIC_TRANSCRIPTION_FIELD] == ""
+    assert editor.note.fields[settings.DEFINITION_FIELD] == "definition"
+    assert editor.note.fields[settings.PRONUNCIATION_FIELD] == "pronunciation"
+    assert editor.note.fields[settings.PHONETIC_TRANSCRIPTION_FIELD] == "transcription"
 
-def test_can_get_pronunciation_hombre():
-    settings.MERRIAM_WEBSTER_API_KEY = SPANISH_KEY
-    settings.PREFERRED_DICTIONARY = "SPANISH"
-    word = "hombre"
-    cardBuilder = cardbuilder.SpanishCardBuilder(word)
-    cardBuilder.addPronunciation()
-    card = cardBuilder.getCard()
 
-    editor = MockEditor()
-    card.serialize(editor)
-
-    assert word == editor.note.fields[0]
-    assert editor.note.fields[settings.PRONUNCIATION_FIELD].find("not available :(") != -1
-
-def test_can_get_pronunciation_alto():
-    settings.MERRIAM_WEBSTER_API_KEY = SPANISH_KEY
-    settings.PREFERRED_DICTIONARY = "SPANISH"
-    word = "alto"
-    cardBuilder = cardbuilder.SpanishCardBuilder(word)
-    cardBuilder.addPronunciation()
-    card = cardBuilder.getCard()
+@pytest.mark.parametrize("dictionary, word, expected_output", [("SPANISH", "entender", "understand"),
+                                                               ("COLLEGIATE", "insert", "to put or thrust")])
+def test_can_get_definition(dictionary, word, expected_output):
+    settings.PREFERRED_DICTIONARY = dictionary
+    card_builder = None
+    if dictionary == "SPANISH":
+        settings.MERRIAM_WEBSTER_API_KEY = SPANISH_KEY
+        card_builder = cardbuilder.SpanishCardBuilder(word)
+    else:
+        settings.MERRIAM_WEBSTER_API_KEY = COLLEGIATE_KEY
+        card_builder = cardbuilder.CollegiateCardBuilder(word)
+    card_builder.addDefinition()
+    card = card_builder.getCard()
 
     editor = MockEditor()
     card.serialize(editor)
 
-    assert word == editor.note.fields[0]
-    assert editor.note.fields[settings.PRONUNCIATION_FIELD].find("alto") != -1
+    assert editor.note.fields[settings.DEFINITION_FIELD].find(expected_output) != -1
+
+
+@pytest.mark.parametrize("dictionary, word, expected_output", [("SPANISH", "hombre", r"not available"),
+                                                               ("SPANISH", "alto", "^http.+?alto.+?wav$"),
+                                                               ("SPANISH", "entender", "^http.+?enten.+?wav$"),
+                                                               ("COLLEGIATE", "insert", "^http.+?insert.+?wav$"),
+                                                               ])
+def test_can_get_pronunciation(dictionary, word, expected_output):
+    settings.PREFERRED_DICTIONARY = dictionary
+    card_builder = None
+    if dictionary == "SPANISH":
+        settings.MERRIAM_WEBSTER_API_KEY = SPANISH_KEY
+        card_builder = cardbuilder.SpanishCardBuilder(word)
+    else:
+        settings.MERRIAM_WEBSTER_API_KEY = COLLEGIATE_KEY
+        card_builder = cardbuilder.CollegiateCardBuilder(word)
+
+    card_builder.addPronunciation()
+    card = card_builder.getCard()
+
+    editor = MockEditor()
+    card.serialize(editor)
+
+    assert re.match(expected_output, editor.note.fields[settings.PRONUNCIATION_FIELD])
